bf6f9930f95609c6bdea0cb7bd5f6656f86542ee
harshaj28 <62892079+harshaj28@users.noreply.github.com>
2022-02-07 23:38:20 +0530

    first commit

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,46 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
-import db from '../firebase';
-import { useSession } from 'next-auth/client';
-import { useRouter } from 'next/router';
-import fire from '../firebase';
+import { EditorState } from "draft-js";
+import fire from "../firebase";
+import { useRouter } from "next/dist/client/router";
+import { convertFromRaw, convertToRaw } from "draft-js";
+import { useSession } from "next-auth/client";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
 
 const Editor = dynamic(
-  () => import('react-draft-wysiwyg').then(module => module.Editor), {
-  ssr: false,
-})
+  () => import("react-draft-wysiwyg").then((module) => module.Editor),
+  {
+    ssr: false,
+  }
+);
 
-const TextEditor = ({ doc }) => {
+function TextEditor() {
   const [session] = useSession();
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const router = useRouter();
   const { id } = router.query;
 
+  const [snapshot] = useDocumentOnce(
+    fire.firestore().collection("userDocs").doc(session.user.email).collection("docs").doc(id)
+  );
+
+  useEffect(() => {
+    if (snapshot?.data()?.editorState) {
+      setEditorState(
+        EditorState.createWithContent(
+          convertFromRaw(snapshot?.data()?.editorState)
+        )
+      );
+    }
+  }, [snapshot]);
+
   const onEditorStateChange = (editorState) => {
     setEditorState(editorState);
 
-    fire.firestore().collection('userDocs').doc(session?.user.email).collection('docs').doc(id).set({
-      editorState: convertToRaw(editorState.getCurrentContent())
-    }, {
-      merge: true
-    })
-  }
-
-  useEffect(() => {
-    if (doc?.editorState) {
-      setEditorState(EditorState.createWithContent(convertFromRaw(doc?.editorState)))
-    }
-  }, [doc]);
+    fire.firestore().collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .doc(id)
+      .set(
+        {
+          editorState: convertToRaw(editorState.getCurrentContent()),
+        },
+        {
+          merge: true,
+        }
+      );
+  };
 
   return (
-    <div className="bg-[#F8F9FA] min-h-screen pb-1">
+    <div className="bg-[#F8F9FA] min-h-screen pb-16">
       <Editor
         editorState={editorState}
-        toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
-        editorClassName="bg-white shadow-lg max-w-5xl mx-auto border p-10 min-h-screen my-6"
         onEditorStateChange={onEditorStateChange}
+        toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
+        editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
       />
     </div>
   );
--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,76 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'
-import { getSession,signOut } from 'next-auth/client';
-import fire from '../../firebase';
-import Icon from "@material-tailwind/react/Icon"
-import TextEditor from '../../components/TextEditor';
-import Button from '@material-tailwind/react/Button'
-import Login from '../../components/Login';
+import  Button  from "@material-tailwind/react/Button"
+import  Icon  from "@material-tailwind/react/Icon"
+import {useRouter} from "next/dist/client/router"
+import fire from "../../firebase"
+import {useDocumentOnce} from "react-firebase-hooks/firestore"
+import { getSession,signOut,useSession } from "next-auth/client"
+import Login from "../../components/Login"
+import TextEditor from "../../components/TextEditor"
 
-const Doc = ({ session }) => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [doc, setDoc] = useState(null);
 
-  useEffect(() => {
+function Doc({session}) {
+    const router=useRouter();
+    const {id}=router.query;
+    const [snapshot,loadingSnapchot]=useDocumentOnce(fire.firestore().collection('userDocs').doc(session?.user.email).collection
+    ('docs').doc(id))
+    if(!loadingSnapchot&&!snapshot?.data()?.fileName){
+        router.replace('/')
+    }
     if(!session) return <Login/>
-    fire.firestore()
-      .collection('userDocs')
-      .doc(session?.user.email)
-      .collection('docs')
-      .doc(id)
-      .get()
-      .then(data => {
-        if (data.data()) {
-          setDoc({ ...data.data() })
-        } else {
-          router.replace('/')
-        }
-      })
-      .catch(err => aler(err))
-
-  }, [id, session]);
-
-  return (
-    <div>
-      <header className="flex justify-between items-center p-3">
-        <span onClick={() => router.push('/')} className="cursor-pointer text-5xl text-[#2196f3] grid place-items-center">
-          <Icon name="description" size="5xl" />
-        </span>
-
-        <div className="flex-grow px-2">
-          <h2 className="text-gray-600">
-            {doc?.fileName}
-          </h2>
-          <div className="flex items-center text-sm space-x-2 -ml-1 h-8 text-gray-500">
-            <p className="">File</p>
-            <p className="">Edit</p>
-            <p className="">View</p>
-            <p className="">Insert</p>
-            <p className="">Format</p>
-            <p className="">Tools</p>
-          </div>
+    
+    return (
+        <div>
+            <header className="flex justify-between items-center p-3 pb-1">
+                <span className="cursor-pointer" onClick={()=>router.push("/")}>
+                <Icon name="description" size="5xl" color="blue" />
+                </span>
+                <div className="flex-grow px-2">
+                    <h2 className="font-medium text-xl">{snapshot?.data()?.fileName}</h2>
+                    <div className="flex items-center text-sm space-x-1 -ml-1 h-8
+                    text-gray-600">
+                        <p className="option">File</p>
+                        <p className="option">Edit</p>
+                        <p className="option">View</p>
+                        <p className="option">Inset</p>
+                        <p className="option">Format</p>
+                        <p className="option">Tools</p>
+                    </div>
+                </div>
+                <Button
+                color="lightBlue"
+                buttonType="filled"
+                size="regular"
+                className="hidden md:inline-flex h-10 border-0"
+                rounded={false}
+                block={false}
+                iconOnly={false}
+                ripple="Light"
+                >
+                    <Icon name="people" size="md" /> SHARE
+                </Button>
+                <img onClick={signOut} className="rounded-full cursor-pointer h-10 w-10 ml-6" src={session?.user.image} />
+            </header>
+            <TextEditor/>
         </div>
-
-        <div className="idRight hidden md:inline-flex items-center">
-          <Button>
-            <Icon name="people" /> SHARE
-          </Button>
-          <img onClick={signOut} src={session?.user.image} className="cursor-pointer rounded-full h-10 w-10 ml-2" alt="" />
-        </div>
-      </header>
-
-      {doc && <TextEditor doc={doc} />}
-    </div>
-  );
+    )
 }
-
-export default Doc;
-
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-
-  return {
-    props: {session}
-  }
+export default Doc
+export async function getServerSideProps(context){
+    const session = await getSession(context)
+    return{
+        props:{
+            session
+        }
+    }
 }
